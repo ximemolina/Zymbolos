e950be34d5bc5da00baef3ca9f42f6eb11ab3d80
natiorozco <nataliaorozcodelgado@gmail.com>
2026-04-30 22:40:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-"""
-import sys
-from explorador.Explorador import lexer
-from analizador.analizador import Analizador
-
-if len(sys.argv) != 2:
-    print("Uso: python Explorador.py <archivo>")
-    sys.exit(1)
-
-archivo = sys.argv[1]
-
-try:
-    todos_los_tokens = []
-    with open(archivo, "r", encoding="utf-8") as file:
-        for numero_linea, linea in enumerate(file, start=1):
-            tokens_linea = lexer(linea, numero_linea)
-            todos_los_tokens.extend(tokens_linea)
-except FileNotFoundError:
-    print(f"ERROR: No se pudo abrir el archivo '{archivo}'")
-    # sys.exit(1)  para que no se cierre con el primer error.
-"""
 
 import sys
 from explorador.Explorador import lexer
